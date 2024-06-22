3c63f3b5f204db7963c920a6bd3a956520b4f3e7
hasegawow <shota.hasegawa0721@gmail.com>
2024-06-22 13:20:08 +0900

    june 22

--- todoapp/routes/index.js
+++ todoapp/routes/index.js
@@ -2,18 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 let todos = [];
+let tests = [];
 
 router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'ToDo App',
     todos: todos,
+    tests: tests,
   });
 });
 
 router.post('/', function (req, res, next) {
-  const todo = req.body.add;
+  const todo = req.body.todo;
   todos.push(todo);
   res.redirect('/');
 });
+
+router.post('/test', function (req, res, next) {
+  const test = req.body.test;
+  tests.push(test);
+  res.redirect('/');
+});
+
+
 
 module.exports = router;
