c1295a0bc36b19adaf937686bc1cb2df249c220d
hasegawow <shota.hasegawa0721@gmail.com>
2024-09-19 10:22:46 +0900

    10時間頑張ったよ

--- todoapp/routes/tab.js
+++ todoapp/routes/tab.js
@@ -6,9 +6,10 @@ const knex = require('../db/knex');
 
 router.post('/', function (req, res, next) {
     const tabs = req.body.plus;
+    const user_id = req.session.userid;
 
     knex('tabs')
-        .insert({name :tabs})
+        .insert({name :tabs, user_id :user_id})
         .then(function (){
             res.redirect('/');
         })
